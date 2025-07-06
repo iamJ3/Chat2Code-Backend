fa6458a40df1d64664474ddc15d59962514517ab
Jatin Sharma <jatina9410@gmail.com>
2025-07-07 05:09:29 +0530

    updated corse poilicy

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['aichatapp-phi.vercel.app', 'http://localhost:5173']
+  credentials: true,  origin: ['aichatapp-phi.vercel.app', 'http://localhost:5173']
 }));
 
 app.use('/projects', projectsRoutes);
