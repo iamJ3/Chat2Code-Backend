47b8a3225a7ae4893fe0136071251732f1c79b0b
Jatin Sharma <jatina9410@gmail.com>
2025-07-07 21:22:26 +0530

    updatte cors erros fix

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.use(cookieParser());
 app.use(cors({
   credentials: true,  origin: ['aichatapp-phi.vercel.app', 'http://localhost:5173']
 }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // OR your vercel URL
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 
 app.use('/projects', projectsRoutes);
 app.use('/users', userRouter);
