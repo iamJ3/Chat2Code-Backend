130c4e6d471129395f27be822abae32243e2b340
Jatin Sharma <jatina9410@gmail.com>
2025-07-08 00:26:51 +0530

    updated cors

--- app.js
+++ app.js
@@ -15,10 +15,11 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  credentials: true,  origin: ['aichatapp-phi.vercel.app', 'http://localhost:5173']
-}));
-app.use(cors({
-  origin: ['https://aichatapp-phi.vercel.app', 'http://localhost:3000'],
+  origin: [
+    'https://aichatapp-phi.vercel.app',
+    'http://localhost:3000',
+    'http://localhost:5173'
+  ],
   credentials: true
 }));
 
