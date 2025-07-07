5c2c247aeda3573d8d62f8083e7a9366974ad687
Jatin Sharma <jatina9410@gmail.com>
2025-07-08 00:33:02 +0530

    fixed corse rogin isseu for running  code

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(cors({
   credentials: true
 }));
 
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
+
 
 app.use('/projects', projectsRoutes);
 app.use('/users', userRouter);
